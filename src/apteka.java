d88fcc9b55ff751110a0eaefd7a9a8f27b3bbddc
sudoku <75527480+sudoku838@users.noreply.github.com>
2024-11-19 15:41:24 +0300

    Add files via upload

---
+++ src/apteka.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Apteka {
+    private String name; // аптека
+    private drugs; // таблетки
+    private int price; // цена
+
+    private static String[] names = {
+            "Артроверон", "Авена Сатива", "Ангелика Форте", "Аспаркам",
+            "Альфа 420", "Амикация"
+    };
+    public Apteka(String name, String drugs, String int price){
+    this.name = name;
+    this.drugs.add(drugs);
+    this.price.add(price);
+    }
+}
